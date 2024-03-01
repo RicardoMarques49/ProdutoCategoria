1b0a0772fdfd5d8131df25b0444340802937e84a
ALUNO <ricardoimep@gmail.com>
2024-02-29 22:33:30 -0300

    Final Aula dia 29_02_2024

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import uuid
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, relationship
-from sqlalchemy import Column, Uuid, String, DateTime, func, DECIMAL, Integer, Boolean, Foreignkey
-
-
-
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import DeclarativeBase, relationship, Session
+from sqlalchemy import Column, Uuid, String, DateTime, func, DECIMAL, Integer, Boolean, ForeignKey
 
 motor = create_engine("sqlite+pysqlite:///banco_de_dados.sqlite", echo=True)
 
@@ -13,30 +10,21 @@ motor = create_engine("sqlite+pysqlite:///banco_de_dados.sqlite", echo=True)
 class Base(DeclarativeBase):
     pass
 
+
 class DatasMixin():
     dta_cadastro = Column(DateTime,
-                          server_default=func.now(),
-                          nullable=False)
+                          server_default=func.now(), nullable=False)
 
-    dta_atualizacao = Column(DateTime,
-                             onupdate=func.now(),
-                             nullable=False)
+    dta_atualizacao = Column(DateTime, onupdate=func.now(), default=func.now(), nullable=False)
 
 
 class Categoria(Base, DatasMixin):
     __tablename__ = 'tbl_categorias'
-    id = Column (Uuid (as_uuid=True),
-                 primary_key=True,
-                 default=uuid.uuid4)
-    nome = Column(String(256),
-                  nullable=False)
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    nome = Column(String(256), nullable=False)
 
     lista_de_produtos = relationship("Produto", back_populates="categoria",
                                      cascade="all, delete-orphan", lazy="selectin")
-
-
-
-
 
 
 class Produto(Base):
@@ -47,12 +35,31 @@ class Produto(Base):
                 default=uuid.uuid4)
     nome = Column(String(256),
                   nullable=False)
-    preco = Column(DECIMAL (10,2), default=0.00)
+    preco = Column(DECIMAL(10, 2), default=0.00)
     estoque = Column(Integer, default=0)
     ativo = Column(Boolean, default=True)
-    categoria_id = Column(Uuid(as_uuid=True), Foreignkey("tbl_categorias.id"))
+    categoria_id = Column(Uuid(as_uuid=True), ForeignKey("tbl_categorias.id"))
 
     categoria = relationship("Categoria", back_populates="lista_de_produtos")
+
+cat = Categoria()
+cat.nome = "Bebidas"
+
+prod = Produto()
+prod.nome = ("Coca cola zero, 2L ")
+prod.ativo = True
+prod.preco = 9.50
+prod.estoque = 100
+prod.categoria = cat
+
+with Session(motor) as sessao:
+     sessao.add(prod)
+     sessao.commit()
+
+with Session(motor) as sessao:
+    categoria = sessao.execute(select(Categoria).where(Categoria.nome == "Bebidas")).scalars()
+    for categoria in categoria:
+        print(f" A categoria {categoria.nome} tem {len(categoria.lista_de_produtos)} produtos")
 
 
 
